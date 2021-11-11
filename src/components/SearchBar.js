272aae42af8353a62ffa25b7f9553be24722965a
Sam Okpara <samuelopra@gmail.com>
2021-11-11 10:05:56 -0500

    initial search commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { searchTerm: "" };
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={(e) => this.setState({ searchTerm: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
